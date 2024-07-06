9b652f98127d0089076dbb126baffeb0be0afa88
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-07-05 22:07:36 -0300

    feat: :sparkles: Criar tabelas com default, inserir dados com default e importar CSV.

--- course-03/sql/comandos.sql
+++ course-03/sql/comandos.sql
@@ -1,3 +1,12 @@
 -- ###################$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 -- ################### ETAPA 01 $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 -- ###################$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+-- Consultando tabelas dos bancos
+
+SELECT * from clientes;
+SELECT * from colaboradores;
+SELECT * from fornecedores;
+SELECT * from produtos;
+SELECT * from pedidos;
+SELECT * from itenspedidos;
