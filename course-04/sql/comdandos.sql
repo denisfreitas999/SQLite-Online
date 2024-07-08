f60e0960242ee46853e36b1251b7d06224ba6ff4
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-07-08 14:18:56 -0300

    feat: :sparkles: Carregando .db no SQLiteOnline, entendendo schema e consultando colunas com *, contando registros com COUNT.

---
+++ course-04/sql/comdandos.sql
@@ -0,0 +1,27 @@
+-- ###################$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+-- ################### ETAPA 01 $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+-- ###################$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+-- Verificando tabelas
+SELECT * from categorias;
+SELECT * from clientes;
+SELECT * from fornecedores;
+SELECT * from marcas;
+SELECT * from produtos;
+select * from vendas;
+
+-- Quantidade de dados por tabela
+SELECT COUNT(*) as Qtd, 'Categorias' as Tabela FROM categorias
+UNION ALL
+SELECT COUNT(*) as Qtd, 'Clientes' as Tabela FROM clientes
+UNION ALL
+SELECT COUNT(*) as Qtd, 'Fornecedores' as Tabela FROM fornecedores
+UNION ALL
+SELECT COUNT(*) as Qtd, 'ItensVenda' as Tabela FROM itens_venda
+UNION ALL
+SELECT COUNT(*) as Qtd, 'Marcas' as Tabela FROM marcas
+UNION ALL
+SELECT COUNT(*) as Qtd, 'Produtos' as Tabela FROM produtos
+UNION ALL
+SELECT COUNT(*) as Qtd, 'Vendas' as Tabela FROM vendas;
+
